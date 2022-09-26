e27c59287bc0546455f831f7e69936862bbbfc51
ayan <ayanalam@cedcoss.com>
2022-09-26 13:02:08 +0530

    variable exercise - 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-// variable excercise -1 
+// variable exercise -1 
 const carName = "Volvo";
 document.getElementById('demo1').innerHTML = carName
+
+// variable exercise - 2"
+var x = 5;
+document.getElementById('demo2').innerHTML = x;
+
+
